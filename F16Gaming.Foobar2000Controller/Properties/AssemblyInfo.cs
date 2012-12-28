8d821463c4bbf555b735745fb48028f6148a0dec
F16Gaming <adam.hellberg95@gmail.com>
2012-12-28 18:10:18 +0100

    Initial commit

---
+++ F16Gaming.Foobar2000Controller/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("F16Gaming foobar2k Controller")]
+[assembly: AssemblyDescription("Provides tools to control and listen to an instance of foobar2k with foo_controlserver installed")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("F16Gaming")]
+[assembly: AssemblyProduct("F16Gaming.Foobar2000Controller")]
+[assembly: AssemblyCopyright("Copyright © 2012 by Adam Hellberg ")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe467843-5662-48a4-97ee-2e8b75b25969")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.1806")]
+[assembly: AssemblyFileVersion("1.0.1.1806")]
